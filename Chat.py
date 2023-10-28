bf1dd5e87244a4997b7d15c8952a78044b00c997
durffy <connor_duffy@hotmail.com>
2023-10-28 09:05:11 -0700

    deleted:    AudioHandler.py 	modified:   Chat.py 	modified:   MeetingMinutes.py 	deleted:    Summarizer.py 	deleted:    TranscriptionHandler.py 	new file:   config/config.json 	new file:   config/prompts.json

--- Chat.py
+++ Chat.py
@@ -1,38 +1,22 @@
-from gpt4all import GPT4All
+import openai
 
 class Chat():
 
-    @classmethod
-    def segment_text(cls, text, max_tokens=2048, overlap=50):
-        words = text.split()
-        segments = []
-        current_segment = []
 
-        for word in words:
-            if len(' '.join(current_segment + [word])) < max_tokens:
-                current_segment.append(word)
-            else:
-                segments.append(' '.join(current_segment))
-                current_segment = current_segment[-overlap:] + [word]
-
-        if current_segment:
-            segments.append(' '.join(current_segment))
-
-        return segments
 
     @classmethod
-    def chat(cls, systemPrompt, prompt):
-        model = GPT4All("orca-mini-3b.ggmlv3.q4_0.bin")
-
-        model.config['systemPrompt'] = systemPrompt
-
-        # If the prompt is short enough, process it directly
-        if len(prompt.split()) < 2048:
-            return model.generate(prompt, max_tokens=2048)
-        
-        # Otherwise, segment the prompt and process each segment
-        segments = cls.segment_text(prompt)
-        responses = [model.generate(segment, max_tokens=2048) for segment in segments]
-        
-        # Combine the responses
-        return ' '.join(responses)
+    def chat(cls, system_prompt, prompt):
+        response = openai.ChatCompletion.create(
+            model="gpt-4",
+            messages=[
+                {
+                    "role": "system",
+                    "content": system_prompt
+                },
+                {
+                    "role": "user",
+                    "content": prompt
+                }
+            ]
+        )
+        return response
--- MeetingMinutes.py
+++ MeetingMinutes.py
@@ -1,43 +1,4 @@
-from datetime import datetime
-import os
-#from AudioHandler import AudioHandler
-from Summarizer import Summarizer
-from TranscriptionHandler import TranscriptionHandler
-
-def process_audio_file(audio_path):
-
-    date_str = datetime.now().strftime("%Y-%m-%d")
-    transcription_file_path = f"{date_str}_transcription.txt"
-
-    # Check if the transcription file already exists
-    if not os.path.exists(transcription_file_path):
-        full_transcription = TranscriptionHandler.transcribe_audio(audio_path)
-        with open(transcription_file_path, "w") as file:
-            file.write(full_transcription)
-    else:
-        # If the file exists, read its content into full_transcription
-        with open(transcription_file_path, "r") as file:
-            full_transcription = file.read()
-
-
-    summary_file_path = f"{date_str}_full_summary.txt"  
-    if not os.path.exists(summary_file_path):
-        full_transcription = TranscriptionHandler.postprocess_transcription(full_transcription)
-
-        # Summarize the entire transcription
-        summary = Summarizer.meeting_minutes(full_transcription)
-        
-        
-        # Save the full summary
-        with open(summary_file_path, "w") as file:
-            file.write(summary)
-    else:
-        # If the file exists, read its content into full_transcription
-        with open(summary_file_path, "r") as file:
-            full_transcription = file.read()
-
-
-
-# Example usage
-audio_path = "Audio_08_14_2023_17_27_30.mp3"  # Modify as needed
-process_audio_file(audio_path)
+# Note: you need to be using OpenAI Python v0.27.0 for the code below to work
+import openai
+audio_file= open("Journal.m4a", "rb")
+transcript = openai.Audio.translate("whisper-1", audio_file)
